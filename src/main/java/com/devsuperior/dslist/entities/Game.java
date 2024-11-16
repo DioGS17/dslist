9d3fb8f4f037a86c9d55806e1623d0fc7f7abf3a
DioGS17 <dioguinhogsantos@gmail.com>
2024-11-15 23:24:06 -0400

    Game seeds

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,30 +20,32 @@ public class Game {
     private String title;
 
     @Column(name = "game_year")
-    private int year;
+    private Integer year;
 
     private String genre;
 
-    private String plataforms;
+    private String platforms;
 
-    private double score;
+    private Double score;
 
     private String imgUrl;
 
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
-    
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
     }
 
-    public Game(long id, String title, int year, String genre, String plataforms, double score, String imgUrl,
+    public Game(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataforms = plataforms;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -66,11 +68,11 @@ public class Game {
         this.title = title;
     }
 
-    public int getYear() {
+    public Integer getYear() {
         return year;
     }
 
-    public void setYear(int year) {
+    public void setYear(Integer year) {
         this.year = year;
     }
 
@@ -82,19 +84,19 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataforms() {
-        return plataforms;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataforms(String plataforms) {
-        this.plataforms = plataforms;
+    public void setPltaforms(String platforms) {
+        this.platforms = platforms;
     }
 
-    public double getScore() {
+    public Double getScore() {
         return score;
     }
 
-    public void setScore(double score) {
+    public void setScore(Double score) {
         this.score = score;
     }
 
